1e4746f1e89c75159f2f3ef018bca7b22fcda0b9
Amandi <119551535+nimashaklaa@users.noreply.github.com>
2026-08-01 06:01:48 +0530

    feat: Integrate PostgreSQL connection and database migrations setup

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,14 +1,54 @@
 package main
 
 import (
+	"database/sql"
+	"errors"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/lib/pq"
 )
 
 func main() {
+	dbURL := os.Getenv("DATABASE_URL")
+	if dbURL == "" {
+		dbURL = "postgres://postgres:password@localhost:5432/train_booking?sslmode=disable"
+	}
+
+	// Connect to database
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("Failed to open DB connection: %v", err)
+	}
+	defer func(db *sql.DB) {
+		if err := db.Close(); err != nil {
+			log.Printf("Error closing database connection: %v\n", err)
+		}
+	}(db)
+
+	if err := db.Ping(); err != nil {
+		log.Fatalf("Failed to ping DB: %v", err)
+	}
+	log.Println("Successfully connected to PostgreSQL database")
+
+	// Run database migrations
+	log.Println("Running database migrations...")
+	m, err := migrate.New("file://migrations", dbURL)
+	if err != nil {
+		log.Fatalf("Failed to create migrate instance: %v", err)
+	}
+	if err := m.Up(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		log.Fatalf("Failed to run migrations: %v", err)
+	}
+	log.Println("Migrations applied successfully")
+
+	// Set up router
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
--- backend/go.mod
+++ backend/go.mod
@@ -3,10 +3,9 @@ module github.com/nimashaklaa/train-seat-booking
 go 1.26.5
 
 require (
-	github.com/go-chi/chi/v5 v5.3.1 // indirect
-	github.com/jackc/pgpassfile v1.0.0 // indirect
-	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
-	github.com/jackc/pgx/v5 v5.10.0 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
-	golang.org/x/text v0.29.0 // indirect
+	github.com/go-chi/chi/v5 v5.3.1
+	github.com/golang-migrate/migrate/v4 v4.19.1
+	github.com/lib/pq v1.12.3
 )
+
+require github.com/stretchr/testify v1.11.1 // indirect
